510d3c7d6265efa36bca85f21194a95f82049d10
yohan_mr <yohanmri.21@uom.lk>
2025-03-01 19:25:35 +0530

    Added CRUD functionality

--- base/urls.py
+++ base/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     #pk means primary key , str means str, int etx can be used
     path('rooms/<str:pk>/',views.room, name="room"),
     path('class/',views.clas, name="classes"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
+    
+    
 ]
